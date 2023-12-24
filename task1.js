ee3b073cd272fc1dc02f2f29b80402f168590f15
davydenko919 <davydenko919@gmail.com>
2023-12-24 11:52:03 +0200

    s1

--- task1.js
+++ task1.js
@@ -1,7 +1,24 @@
-function makeTransaction(quantity, pricePerDroid){
-    return `You ordered ${quantity} droids worth ${quantity*pricePerDroid} credits!`;
+// const start = 6;
+// const end = 17;
+// let number;
+
+// for (number = start; number == end; number++){
+//   if (number % 5 === 0){
+//     break;
+//   }
+// }
+
+// console.log(number);
+
+const start = 6;
+const end = 17;
+let number;
+
+for (let i = start; i <= end; i++) {
+    if (i % 5 === 0) {
+        number = i;
+        break;
+    }
 }
 
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"   
+console.log(number); // Результат буде 10
