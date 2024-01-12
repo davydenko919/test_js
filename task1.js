05a462e47f45227734ea4ed4ae924be796df495f
davydenko919 <davydenko919@gmail.com>
2024-01-12 15:13:05 +0200

    1

--- task1.js
+++ task1.js
@@ -1,20 +1,7 @@
-const atTheOldToad = {
-    potions: [
-      { name: "Speed potion", price: 460 },
-      { name: "Stone skin", price: 520 },
-    ],
-    getPotions() {
-      return this.potions;
-    },
-    addPotion(newPotion) {
-      this.potions.push(newPotion);
-    },
-    getTotalPrice() {
-      let sum = 0;
-      for(const potion of potions){
-        sum += potion.price;
-      };
-      return sum;
-    },
-  };
-  
+function add(...args) {
+  const sum = 0;
+  for (const el of args) {
+    sum += el;
+  }
+  return sum;
+}
